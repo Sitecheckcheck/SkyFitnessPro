47276a66733ba772a3360d533bb9e79de3111a9a
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-11-12 21:51:53 +0700

    teamwork

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Tanya from './views/tanya';
+import Lesia from './views/lesia';
+import Pasha from './views/pasha';
+import Nikita from './views/nikita';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Tanya />
+      <Lesia />
+      <Pasha />
+      <Nikita />
     </div>
   );
 }
---
+++ src/views/pasha.js
@@ -0,0 +1,5 @@
+export default function Pasha() {
+  return (
+    <div></div>
+  );
+}
