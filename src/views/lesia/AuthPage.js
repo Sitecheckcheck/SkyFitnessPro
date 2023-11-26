b974ee82128cb0d2b87fd29d8ab7b00aacb54949
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-11-26 13:52:59 +0700

    fixed repeat password

--- src/views/lesia/AuthPage.js
+++ src/views/lesia/AuthPage.js
@@ -3,13 +3,16 @@ import { Button } from "./Button";
 import { useDispatch } from "react-redux";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { setUser } from "../../store/slices/userSlice";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import styles from "./Form.module.css"
 
 export const AuthPage = ({isLoginMode}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isSubmiting, setIsSubmiting] = useState(false);
 
   const auth = getAuth();
 
@@ -17,8 +20,15 @@ export const AuthPage = ({isLoginMode}) => {
   const dispatch = useDispatch();
 
   const handleLogin = () =>{
+    if (!email || !password) {
+      setError('Укажите почту/пароль');
+      return;
+    }
+    
+    setIsSubmiting(true);
     signInWithEmailAndPassword(auth, email, password)
     .then(({user}) => {
+      setIsSubmiting(false);
       console.log(user)
       dispatch(setUser({
         email: user.email,
@@ -28,11 +38,26 @@ export const AuthPage = ({isLoginMode}) => {
       }))
       navigate('/profile');
     })
-    .catch(console.error);
-    
+    .catch((error) => {
+      setIsSubmiting(false);
+      setError(error.message)}
+    )
   };
 
   const handleSignin = () =>{
+    if (!email || !password) {
+      setError('Укажите почту/пароль');
+      return;
+    }
+
+    if (password!==repeatPassword) {
+      setError('Пароли не совпадают');
+      return;
+    }
+
+    setIsSubmiting(true);
+    setError('');
+
     createUserWithEmailAndPassword(auth, email, password)
     .then(({user}) => {
       dispatch(setUser({
@@ -41,10 +66,18 @@ export const AuthPage = ({isLoginMode}) => {
         token: user.accessToken,
         pass: password,
       }))
+      setIsSubmiting(false);
       navigate('/profile');
     })
-    .catch(console.error)
+    .catch((error) => {
+      setError(error.message);
+      setIsSubmiting(false);
+    })
   };
+
+  useEffect(() => {
+    setError(null);
+  }, [isLoginMode, email, password, repeatPassword]);
 
   return (
     <div className={styles.page}>
@@ -69,12 +102,20 @@ export const AuthPage = ({isLoginMode}) => {
             onChange={(event) => {setPassword(event.target.value)}}
           />
           { !isLoginMode && (
-            <input className={styles.inputForm} type="password" name="password" placeholder="Повторите пароль"></input>
+            <input className={styles.inputForm}
+            type="password"
+            name="password"
+            placeholder="Повторите пароль"
+            value={repeatPassword}
+            onChange={(event) => {setRepeatPassword(event.target.value)}}
+          />
           )}
         </div>
+        {error && <div className={styles.inputError}>{error}</div>}
+
         {isLoginMode ? (
           <>
-            <Button text="Войти" onClick={() => handleLogin(email, password)}/>
+            <Button text={isSubmiting ? "Логинимся" : "Войти"} onClick={() => handleLogin(email, password)}/>
             <NavLink to="/register" className={styles.buttonSignup}>
               Зарегистрироваться
             </NavLink>
