c0e1f11c49b9b8812fbc634601540e72c91b3f8a
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-11-27 22:51:20 +0300

    popup-menu component created and used on workout-video page

--- src/views/nikita/workout-video-page.js
+++ src/views/nikita/workout-video-page.js
@@ -1,24 +1,13 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
 import { createPortal } from "react-dom";
-import { useNavigate } from "react-router-dom";
-import { removeUser } from "../../store/slices/userSlice";
 import { Progress } from "../lesia/Progress";
 import { ProgressCheck } from "../lesia/ProgressCheck";
 import { Modal } from "../lesia/Modal";
+import { Popupmenu } from "../../components/popup-menu";
 import "./workout-video-page.css";
 //
 export const WorkoutVideoPage = ({ login }) => {
-  const dispatch = useDispatch();
-
-  const navigate = useNavigate();
-
   const [progress, setProgress] = useState("");
-
-  const [visibleFilter, setVisibleFilter] = useState(null);
-  const toggleVisibleFilter = (filter) => {
-    setVisibleFilter(visibleFilter === filter ? null : filter);
-  };
 
   const getModalForm = () => {
     switch (progress) {
@@ -41,46 +30,7 @@ export const WorkoutVideoPage = ({ login }) => {
       <div className="wrapper">
         <div className="logo-wrapper">
           <div className="logo" />
-          <div
-            className="user-wrapper"
-            onClick={() => toggleVisibleFilter("popup")}
-          >
-            <div className="user-avatar" />
-            <div className="user-name">{login}</div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="9"
-              viewBox="0 0 14 9"
-              fill="none"
-            >
-              <path
-                d="M12.3553 1.03308L6.67773 6.7107L1.00012 1.03308"
-                stroke="black"
-                strokeWidth="2"
-              />
-            </svg>
-            {visibleFilter === "popup" && (
-              <ul className="popup" $popup>
-                <div className="popup-box" $popup>
-                  <li className="popup-line">На главную</li>
-                  <li className="popup-line">Профиль</li>
-                  <li
-                    className="popup-line"
-                    onClick={() => {
-                      dispatch(removeUser());
-                      localStorage.removeItem("email");
-                      localStorage.removeItem("id");
-                      localStorage.removeItem("token");
-                      navigate("/");
-                    }}
-                  >
-                    Выйти
-                  </li>
-                </div>
-              </ul>
-            )}
-          </div>
+          <Popupmenu login={login} />
         </div>
         <div className="content-wrapper">
           <h1 className="header">Йога</h1>
