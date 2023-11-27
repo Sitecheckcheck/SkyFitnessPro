55768e2e1d54f3978199ec295e48d2d32e46dfe9
Pavel <pvdanilov88@inbox.ru>
2023-11-27 06:27:55 +0300

    хранение пользователя в локалсторадже localStorage

--- src/views/lesia/AuthPage.js
+++ src/views/lesia/AuthPage.js
@@ -1,16 +1,20 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 import { useDispatch } from "react-redux";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { setUser } from "../../store/slices/userSlice";
 import { useState, useEffect } from "react";
 
-import styles from "./Form.module.css"
+import styles from "./Form.module.css";
 
-export const AuthPage = ({isLoginMode}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+export const AuthPage = ({ isLoginMode }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
   const [error, setError] = useState(null);
   const [isSubmiting, setIsSubmiting] = useState(false);
 
@@ -19,60 +23,70 @@ export const AuthPage = ({isLoginMode}) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const handleLogin = () =>{
+  const handleLogin = () => {
     if (!email || !password) {
-      setError('Укажите почту/пароль');
+      setError("Укажите почту/пароль");
       return;
     }
-    
+
     setIsSubmiting(true);
     signInWithEmailAndPassword(auth, email, password)
-    .then(({user}) => {
-      setIsSubmiting(false);
-      console.log(user)
-      dispatch(setUser({
-        email: user.email,
-        id: user.uid,
-        token: user.accessToken,
-        pass: password,
-      }))
-      navigate('/profile');
-    })
-    .catch((error) => {
-      setIsSubmiting(false);
-      setError(error.message)}
-    )
+      .then(({ user }) => {
+        setIsSubmiting(false);
+        console.log(user);
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken,
+            pass: password,
+          })
+        );
+        localStorage.setItem("email", user.email);
+        localStorage.setItem("id", user.uid);
+        localStorage.setItem("token", user.accessToken);
+        navigate("/profile");
+      })
+      .catch((error) => {
+        setIsSubmiting(false);
+        setError(error.message);
+      })
   };
 
-  const handleSignin = () =>{
+  const handleSignin = () => {
     if (!email || !password) {
-      setError('Укажите почту/пароль');
+      setError("Укажите почту/пароль");
       return;
     }
 
-    if (password!==repeatPassword) {
-      setError('Пароли не совпадают');
+    if (password !== repeatPassword) {
+      setError("Пароли не совпадают");
       return;
     }
 
     setIsSubmiting(true);
-    setError('');
+    setError("");
 
     createUserWithEmailAndPassword(auth, email, password)
-    .then(({user}) => {
-      dispatch(setUser({
-        email: user.email,
-        id: user.uid,
-        token: user.accessToken,
-        pass: password,
-      }))
-      setIsSubmiting(false);
-      navigate('/profile');
-    })
-    .catch((error) => {
-      setError(error.message);
-      setIsSubmiting(false);
-    })
+      .then(({ user }) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken,
+            pass: password,
+          })
+        );
+        setIsSubmiting(false);
+        localStorage.setItem("email", user.email);
+        localStorage.setItem("id", user.uid);
+        localStorage.setItem("token", user.accessToken);
+        navigate("/profile");
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsSubmiting(false);
+      });
   };
 
   useEffect(() => {
@@ -88,41 +102,54 @@ export const AuthPage = ({isLoginMode}) => {
         <div className={styles.inputBox}>
           <input
             className={styles.inputForm}
-            type="text" name="login"
+            type="text"
+            name="login"
             placeholder="Логин"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
-          <input 
+          <input
             className={styles.inputForm}
             type="password"
             name="password"
             placeholder="Пароль"
             value={password}
-            onChange={(event) => {setPassword(event.target.value)}}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
           />
-          { !isLoginMode && (
-            <input className={styles.inputForm}
-            type="password"
-            name="password"
-            placeholder="Повторите пароль"
-            value={repeatPassword}
-            onChange={(event) => {setRepeatPassword(event.target.value)}}
-          />
+          {!isLoginMode && (
+            <input
+              className={styles.inputForm}
+              type="password"
+              name="password"
+              placeholder="Повторите пароль"
+              value={repeatPassword}
+              onChange={(event) => {
+                setRepeatPassword(event.target.value);
+              }}
+            />
           )}
         </div>
         {error && <div className={styles.inputError}>{error}</div>}
 
         {isLoginMode ? (
           <>
-            <Button text={isSubmiting ? "Логинимся" : "Войти"} onClick={() => handleLogin(email, password)}/>
+            <Button
+              text={isSubmiting ? "Логинимся" : "Войти"}
+              onClick={() => handleLogin(email, password)}
+            />
             <NavLink to="/register" className={styles.buttonSignup}>
               Зарегистрироваться
             </NavLink>
           </>
-        ) : (<Button text="Зарегистрироваться" onClick={() => handleSignin(email, password)}/>)
-        }
+        ) : (
+          <Button
+            text="Зарегистрироваться"
+            onClick={() => handleSignin(email, password)}
+          />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
